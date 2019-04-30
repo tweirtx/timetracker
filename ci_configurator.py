49e2244a2307416e8bcb1f06d1bd27f67c95c7d9
Travis Weir <tweirtx@gmail.com>
2019-04-30 13:00:28 -0500

    Postgres?

--- ci_configurator.py
+++ ci_configurator.py
@@ -7,18 +7,20 @@ config = {
     'signout_code': "000000",
     'minimum_hours': 0
 }
+if os.getenv("TRAVIS"):
 
-if os.getenv("DB_TYPE") == "sqlite":
-    print("Nothing to do!")
-elif os.getenv("DB_TYPE") == "postgres":
-    print("Configuring postgres")
-    subprocess.call('sudo -u postgres psql "CREATE USER timetracker; CREATE DATABASE timetracker;"', shell=True)
-    config['db_url'] = "postgres://timetracker:timetracker@localhost/timetracker"
-    print("postgres is configured")
-elif os.getenv('DB_TYPE') == "mysql":
-    print("Configuring mysql")
-else:
-    print("I can't handle this!")
+    if os.getenv("DB_TYPE") == "sqlite":
+        print("Nothing to do!")
+    elif os.getenv("DB_TYPE") == "postgres":
+        print("Configuring postgres")
+        # subprocess.call('sudo -u postgres psql "CREATE USER timetracker; CREATE DATABASE timetracker;"', shell=True)
+        config['db_url'] = "postgres://timetracker:timetracker@localhost/timetracker"
+        print("postgres is configured")
+    elif os.getenv('DB_TYPE') == "mysql":
+        print("Configuring mysql")
+    else:
+        print("I can't handle this!")
+
 
 with open('config.json', 'w') as f:
     f.write(json.dumps(config))
