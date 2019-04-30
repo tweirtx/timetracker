f6f85d49c3afcc0c2cd0aceb27e579f9ce3b6e2f
Travis Weir <tweirtx@gmail.com>
2019-04-30 11:18:24 -0500

    Reports

--- timetracker/web.py
+++ timetracker/web.py
@@ -1,8 +1,25 @@
 import os
 from .signer import sign
+from .db import *
+from .config import config
 import flask
+from flask_table import Table, Col
 
 app = flask.Flask('timetracker')
+
+
+class ReportTable(Table):
+
+    name = Col('Name')
+    hours = Col('Hours')
+    has_made_hours = Col('Has made hours')
+
+
+class Report(object):
+    def __init__(self, user_report):
+        self.name = user_report.name
+        self.hours = user_report.minutes / 60
+        self.has_made_hours = self.hours >= config['minimum_hours']
 
 
 @app.route('/')
@@ -20,7 +37,12 @@ def execute():
 
 @app.route('/report')
 def report():
-    return "TODO"
+    report_objects = []
+    with Session() as session:
+        reports = session.query(Members)
+        for user_report in reports:
+            report_objects.append(Report(user_report))
+    return ReportTable(report_objects, border=True).__html__()
 
 
 @app.route('/submit.js')
