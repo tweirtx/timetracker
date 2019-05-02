4a85e87bfa302649f5b652c5f84020e6125c27a4
Travis Weir <tweirtx@gmail.com>
2019-05-02 00:50:44 -0500

    grnat

--- ci_mysql.sql
+++ ci_mysql.sql
@@ -1,4 +1,5 @@
 CREATE DATABASE timetracking;
 USE timetracking;
 CREATE USER timetracker IDENTIFIED BY 'timetracker';
-GRANT ALL ON timetracking TO timetracker;
+GRANT ALL ON timetracking.students TO timetracker;
+GRANT ALL ON timetracking.verboselogs TO timetracker;
