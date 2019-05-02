5afb52dd92a3d821b7893b941fefa97de88cfe92
Travis Weir <tweirtx@gmail.com>
2019-05-02 14:11:02 -0500

    Autocommit

--- timetracker/test.py
+++ timetracker/test.py
@@ -3,16 +3,15 @@ import time
 
 
 def run():
-    with db.Session() as session:
+    with db.Session(autocommit=True) as session:
 
         session.add(db.Members(user_id="test01", name="Test01"))
         session.add(db.Members(user_id="test02", name="Test02"))
-        session.commit()
 
-        time.sleep(1)
+        #time.sleep(1)
 
         result = signer.sign("test01")
-        print(result)
+        #print(result)
         if result == "Successfully signed Test01 in" and session.query(db.Members).\
                 filter_by(user_id='test01', signed_in=True).one_or_none():
             print("Sign-in test01 success")
@@ -20,28 +19,28 @@ def run():
             print("ERROR: Sign-in test01 failed!")
             exit(1)
 
-        time.sleep(1)
-        session.commit()
+        #time.sleep(1)
+
         result = signer.sign("test01")
-        print(result)
-        time.sleep(1)
+        #print(result)
+        #time.sleep(1)
         query_result = session.query(db.Members).filter_by(user_id='test01', signed_in=False).one_or_none()
-        print(query_result)
-        print("test01 exists in DB: ", session.query(db.Members).filter_by(user_id='test01', signed_in=False).one_or_none())
+        #print(query_result)
+        #print("test01 exists in DB: ", session.query(db.Members).filter_by(user_id='test01', signed_in=False).one_or_none())
         if result == "Successfully signed Test01 out" and query_result:
             print("Sign-out test01 success")
         else:
             print("ERROR: Sign-out test01 failed!")
             exit(1)
 
-        time.sleep(1)
+        #time.sleep(1)
 
         signer.sign("test01")
 
-        time.sleep(1)
+        #time.sleep(1)
 
         result = signer.sign("test02")
-        print(result)
+        #print(result)
         if result == "Successfully signed Test02 in" and session.query(db.Members).\
                 filter_by(user_id='test02', signed_in=True).one_or_none():
             print("Sign-in test02 success")
@@ -49,10 +48,10 @@ def run():
             print("ERROR: Sign-in test02 failed!")
             exit(1)
 
-        time.sleep(1)
+        #time.sleep(1)
 
         result = signer.sign("000000")
-        print(result)
+        #print(result)
         if result == "Successfully signed out 2 members!" and session.query(db.Members). \
                 filter_by(user_id='test01', signed_in=False).one_or_none() and session.query(db.Members). \
                 filter_by(user_id='test02', signed_in=False).one_or_none():
