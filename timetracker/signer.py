6099f29f0d8582995a3cf042cbb248c2f0040087
Travis Weir <tweirtx@gmail.com>
2019-05-02 11:34:30 -0500

    derp

--- timetracker/signer.py
+++ timetracker/signer.py
@@ -16,14 +16,14 @@ def sign(user_id):
         mem = session.query(Members).filter_by(user_id=user_id).one_or_none()
         if not mem:
             return "Error: Member not found"
-        if mem.signed_in or mem.signed_in == 1:
+        elif mem.signed_in or mem.signed_in == 1:
             mem.minutes += ((datetime.datetime.now().timestamp() - mem.sign_in_time.timestamp()) / 60)
             mem.sign_in_time = None
             mem.signed_in = False
             session.add(VerboseLogs(user_id=mem.user_id, signing_in=False, current_datetime=datetime.datetime.now()))
             session.commit()
             return "Successfully signed {} out".format(mem.name)
-        if not mem.signed_in or mem.signed_in == 0:
+        elif not mem.signed_in or mem.signed_in == 0:
             mem.sign_in_time = datetime.datetime.now()
             mem.signed_in = True
             session.add(VerboseLogs(user_id=mem.user_id, signing_in=True, current_datetime=datetime.datetime.now()))
