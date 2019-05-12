7efac9387451325ed0c8c819ec5215188788cc5d
Travis Weir <tweirtx@gmail.com>
2019-05-11 22:06:33 -0500

    I made an error

--- pi.sh
+++ pi.sh
@@ -5,5 +5,5 @@ sudo apt install libpq-dev libmariadbclient-dev freetds-dev -y
 git clone https://github.com/tweirtx/timetracker
 cd timetracker
 pyenv local 3.6.7
-pip3 install Cython --prefer-binary # Cython takes ages to build on pi
+pip3 install --prefer-binary Cython # Cython takes ages to build on pi so we're trying to get a precompiled one
 pip3 install -Ur requirements.txt
