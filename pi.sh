963c6f5f3623a8c36708a10fcec04f74cca31c22
Travis Weir <tweirtx@gmail.com>
2019-05-12 17:38:07 -0500

    Ifs

--- pi.sh
+++ pi.sh
@@ -8,7 +8,8 @@ pip3 install Cython --only-binary :all: # Cython takes ages to build on pi so we
 pip3 install -Ur requirements.txt
 echo "Would you like TimeTracker to start automatically upon boot? [y/n]"
 read autoboot
-if ${autoboot} == 'y'; then
-    echo "echo Starting TimeTracker..." >> .bashrc
-    echo "cd timetracker && python3 -m timetracker" >> .bashrc
+if [[ ${autoboot} == 'y' ]]; then
+    echo "echo Starting TimeTracker..." >> ~/.bashrc
+    echo "cd timetracker && python3 -m timetracker" >> ~/.bashrc
+    echo "Autobooting"
 fi
