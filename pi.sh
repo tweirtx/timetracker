65275f014131bac96463e0594179ac4689a659e1
Travis Weir <tweirtx@gmail.com>
2019-05-11 20:16:06 -0500

    Started a Pi init script, no autoloading yet

---
+++ pi.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+curl pyenv.run | bash
+pyenv install 3.6.7
+sudo apt install libpq-dev libmariadbclient-dev freetds-dev
+git clone https://github.com/tweirtx/timetracker
+cd timetracker
+pyenv local 3.6.7
+pip3 install Cython --prefer-binary # Cython takes ages to build on pi
+pip3 install -Ur requirements.txt
