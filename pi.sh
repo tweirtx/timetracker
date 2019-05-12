be920afcdd09e10b46e0ef6963fadbdf92b87b95
Travis Weir <tweirtx@gmail.com>
2019-05-12 15:29:50 -0500

    Pi bootstrapping script with option

--- pi.sh
+++ pi.sh
@@ -4,5 +4,9 @@ git clone https://github.com/tweirtx/timetracker
 cd timetracker
 pip3 install Cython --prefer-binary :all: # Cython takes ages to build on pi so we're trying to get a precompiled one
 pip3 install -Ur requirements.txt
-echo "echo Starting TimeTracker..."
-echo "cd timetracker && python3 -m timetracker" >> .bashrc
+echo "Would you like TimeTracker to start automatically upon boot? [y/n]"
+read autoboot
+if ${autoboot} == 'y'; then
+    echo "echo Starting TimeTracker..." >> .bashrc
+    echo "cd timetracker && python3 -m timetracker" >> .bashrc
+fi
