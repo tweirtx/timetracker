2fc9fb5e4adf47dc056fac299ee5f0aa1cc330e5
Travis Weir <tweirtx@gmail.com>
2019-05-11 22:21:00 -0500

    ack

--- pi.sh
+++ pi.sh
@@ -1,5 +1,10 @@
 #!/usr/bin/env bash
 curl pyenv.run | bash
+echo "export PATH=\"~/.pyenv/bin:$PATH\"
+eval \"$(pyenv init -)\"
+eval \"$(pyenv virtualenv-init -)\"
+" >> ~/.bashrc
+~/.bashrc
 pyenv install 3.6.7
 sudo apt install libpq-dev libmariadbclient-dev freetds-dev -y
 git clone https://github.com/tweirtx/timetracker
