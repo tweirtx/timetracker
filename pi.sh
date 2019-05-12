c3448e573297f713dc69a044136bca143d93431c
Travis Weir <tweirtx@gmail.com>
2019-05-12 15:20:38 -0500

    Pi bootstrapping script without options/docs

--- pi.sh
+++ pi.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
-sudo apt install libpq-dev libmariadbclient-dev freetds-dev -y
+sudo apt install libpq-dev libmariadbclient-dev freetds-dev python3 python3-pip -y
 git clone https://github.com/tweirtx/timetracker
 cd timetracker
-pip3 install --prefer-binary Cython # Cython takes ages to build on pi so we're trying to get a precompiled one
+pip3 install Cython --prefer-binary :all: # Cython takes ages to build on pi so we're trying to get a precompiled one
 pip3 install -Ur requirements.txt
+echo "echo Starting TimeTracker..."
+echo "cd timetracker && python3 -m timetracker" >> .bashrc
